2de69ed8526bcc6d8f60b2ec03a5c2d9ed41b71d
Igor Afonov <afonov@gmail.com>
2012-07-11 17:21:32 +0300

    Changed test for presense of Chris Lea PPA to work on different versions of Ubuntu.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@ package "python-software-properties"
 
 execute "setup ppa apt repository" do
   command "add-apt-repository ppa:chris-lea/node.js && apt-get update"
-  not_if  "test -f /etc/apt/sources.list.d/chris-lea-node.js-lucid.list"
+  not_if  "ls /etc/apt/sources.list.d/chris-lea*"
 end
 
 package "nodejs"
