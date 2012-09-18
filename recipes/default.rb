6a7a7e719c7884df21aca61ce95d08b24340e3e8
Phil Cohen <github@phlippers.net>
2012-09-18 14:45:56 -0700

    `ls` returns an error if the file doesn't exist

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@ package "python-software-properties"
 
 execute "setup ppa apt repository" do
   command "add-apt-repository ppa:chris-lea/node.js && apt-get update"
-  not_if  "ls /etc/apt/sources.list.d/chris-lea-node*"
+  not_if  "test -f /etc/apt/sources.list.d/chris-lea-node*"
 end
 
 package "nodejs"
