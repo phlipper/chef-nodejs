ac38a9ca42c742cf66572722a0a819bb9ae74425
Phil Cohen <github@phlippers.net>
2012-10-29 21:32:28 -0700

    use ruby block to test file existence

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@ package "python-software-properties"
 
 execute "setup ppa apt repository" do
   command "add-apt-repository ppa:chris-lea/node.js && apt-get update"
-  not_if  "test -f /etc/apt/sources.list.d/chris-lea-node*"
+  not_if  { Dir["/etc/apt/sources.list.d/chris-lea-node*"].any? }
 end
 
 package "nodejs"
