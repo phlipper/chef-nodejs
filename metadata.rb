a90297b1feafaedd02382df8d070bb0f719dce05
Phil Cohen <github@phlippers.net>
2012-12-26 23:23:41 -0800

    bump version to 1.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "github@phlippers.net"
 license           "MIT"
 description       "Installs Node.js"
 long_description  "Please refer to README.md"
-version           "1.2.0"
+version           "1.3.0"
 
 recipe "nodejs", "Sets up the apt repository and installs dependent packages"
 recipe "nodejs::dev", "Install the development libraries"
