bc5eebdfff2b008a4414d0729b1a27126e9f2e13
Phil Cohen <github@phlippers.net>
2015-04-26 08:50:11 -0700

    bump version to 1.8.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "github@phlippers.net"
 license           "MIT"
 description       "Installs Node.js"
 long_description  "Please refer to README.md"
-version           "1.7.1"
+version           "1.8.0"
 
 recipe "nodejs", "Sets up the apt repository and installs dependent packages"
 recipe "nodejs::_apt", "Internal recipe to configure the apt repository"
